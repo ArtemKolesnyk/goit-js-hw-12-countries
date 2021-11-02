153402f3391c37e6cab5384a22eb08158124766e
ArtemKolesnyk <kolesnik2405@gmail.com>
2021-11-02 22:48:49 +0100

    hw-12

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
 import './sass/main.scss';
+import API from './js/fetchCountries';
+
+import countryList from '../templates/countriesList.hbs';
+import countryCard from '../templates/country.hbs';
+
+import '@pnotify/core/dist/BrightTheme.css';
+const { error } = require('@pnotify/core');
+var debounce = require('lodash.debounce');
+
+import getRefs from './js/refs';
+const refs = getRefs();
+refs.inputSearch.addEventListener('input', debounce(onSearch, 500));
+
+function onSearch(e) {
+  e.preventDefault();
+  const searchQuery = e.target.value;
+  hideCountryList();
+
+  API.fetchCountries(searchQuery)
+    .then(onSearchQuery)
+    .catch(error => {
+      console.log(error);
+    });
+}
+function onSearchQuery(searchList) {
+  if (searchList.length > 10) {
+    error({
+      text: 'Too many matches found. Please enter a more specific query!',
+      delay: 250,
+    });
+  } else if (searchList.length === 1) {
+    renderCountryList(searchList, countryCard);
+  } else if (searchList.length <= 10 || searchList.length >= 2) {
+    renderCountryList(searchList, countryList);
+  }
+}
+function renderCountryList(countries, name) {
+  const markup = countries.map(country => name(country)).join(' ');
+  refs.cardContainer.insertAdjacentHTML('beforeend', markup);
+}
+function hideCountryList() {
+  refs.cardContainer.innerHTML = '';
+}
---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+export default function getRefs() {
+    return {
+      cardContainer: document.querySelector('.cardContainer'),
+      inputSearch: document.querySelector('.formSearch'),
+    };
+  }
